c22b4d6caa0126f4b0778a6af1ab58fe91cd0ce5
Kier Davis <kierdavis@gmail.com>
2019-10-23 21:54:36 +0100

    Also remove MARKER_TOKEN and MARKER_ROBOT from __init__.py

--- sr/robot/__init__.py
+++ sr/robot/__init__.py
@@ -2,10 +2,6 @@ from .simulator import Simulator
 from .sim_robot import SimRobot, AlreadyHoldingSomethingException
 from .vision import (
     MARKER_ARENA,
-    MARKER_ROBOT,
-
-    MARKER_TOKEN,
-
     MARKER_TOKEN_GOLD,
     MARKER_TOKEN_SILVER,
 )
